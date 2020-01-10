df66b19d401e14139fb7c9e1fcf9dcf4408869bd
James Robinson <james.em.robinson@gmail.com>
2020-01-10 12:33:46 +0000

    Initial test of action

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # azure-vm-deploy
 GitHub Action for deploying an Azure VM and running scripts on it
+
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+uses: ResearchSoftwareActions/azure-vm-deploy@master
+with:
+  who-to-greet: 'Mona the Octocat'
+```
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+resource_group=$1
+vm_name=$2
+vm_size=$3
+
+echo "Creating VM"
+az vm create --resource-group $resource_group --name $vm_name --image UbuntuLTS --size $vm_size --admin-username githubactionsadmin --generate-ssh-keys --ephemeral-os-disk true
+
+remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts "echo $1 $2 $3" --parameters oh hello world)
+
+# remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts @${{ env.PATH_TO_SCRIPT }} --parameters oh hello world)
+
+
+echo "$remote_output"
+
+stdout=${remote_output%%\\n\\n[stderr*}
+
+
+
+
+    #       remote_output_without_stderr=${remote_output%%\\n\\n[stderr*}
+    #       final_stdout_line=$(echo -e ${remote_output_without_stderr##*stdout]\\n} | tail -n 1)
+    #       echo "$final_stdout_line"
+    #       if [[ "$final_stdout_line" -ne "SUCCESS" ]]; then set -e o pipefail; fi
+
+
+
+echo ::set-output name=stdout::$stdout
