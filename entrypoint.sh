410e90d5831a5dcde3567f82fbd84861e6f301d3
James Robinson <james.em.robinson@gmail.com>
2020-01-10 14:40:57 +0000

    Test passing credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,51 @@
 #! /bin/sh
-resource_group=$1
-vm_name=$2
-vm_size=$3
+credentials=$1
+resource_group=$2
+vm_name=$3
+vm_size=$4
 
+echo "credentials: $credentials"
 echo "resource_group: $resource_group"
 echo "vm_name: $vm_name"
 echo "vm_size: $vm_size"
 
-echo "pwd: $(pwd)"
-echo "/github"
-ls -alh /github
-echo "/github/home"
-ls -alh /github/home
-echo "/github/workspace"
-ls -alh /github/workspace
-
-mv /github/home/.azure /root/.azure
-
-echo "/root"
-ls -alh /root
-echo "/root/.azure"
-ls -alh /root/.azure
+# echo "pwd: $(pwd)"
+# echo "/github"
+# ls -alh /github
+# echo "/github/home"
+# ls -alh /github/home
+# echo "/github/workspace"
+# ls -alh /github/workspace
 
 # mv /github/home/.azure /root/.azure
 
-
-echo "Creating VM"
-az vm create --resource-group $resource_group --name $vm_name --image UbuntuLTS --size $vm_size --admin-username githubactionsadmin --generate-ssh-keys --ephemeral-os-disk true
-
-remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts "echo $1 $2 $3" --parameters oh hello world)
-
-# remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts @${{ env.PATH_TO_SCRIPT }} --parameters oh hello world)
+# echo "/root"
+# ls -alh /root
+# echo "/root/.azure"
+# ls -alh /root/.azure
+# # mv /github/home/.azure /root/.azure
 
 
-echo "$remote_output"
+# echo "Creating VM"
+# az vm create --resource-group $resource_group --name $vm_name --image UbuntuLTS --size $vm_size --admin-username githubactionsadmin --generate-ssh-keys --ephemeral-os-disk true
 
-stdout=${remote_output%%\\n\\n[stderr*}
+# remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts "echo $1 $2 $3" --parameters oh hello world)
+
+# # remote_output=$(az vm run-command invoke --name $vm_name --resource-group $resource_group --command-id RunShellScript --query "value[0].message" --scripts @${{ env.PATH_TO_SCRIPT }} --parameters oh hello world)
 
 
+# echo "$remote_output"
 
-
-    #       remote_output_without_stderr=${remote_output%%\\n\\n[stderr*}
-    #       final_stdout_line=$(echo -e ${remote_output_without_stderr##*stdout]\\n} | tail -n 1)
-    #       echo "$final_stdout_line"
-    #       if [[ "$final_stdout_line" -ne "SUCCESS" ]]; then set -e o pipefail; fi
+# stdout=${remote_output%%\\n\\n[stderr*}
 
 
 
-echo ::set-output name=stdout::$stdout
+
+#     #       remote_output_without_stderr=${remote_output%%\\n\\n[stderr*}
+#     #       final_stdout_line=$(echo -e ${remote_output_without_stderr##*stdout]\\n} | tail -n 1)
+#     #       echo "$final_stdout_line"
+#     #       if [[ "$final_stdout_line" -ne "SUCCESS" ]]; then set -e o pipefail; fi
+
+
+
+# echo ::set-output name=stdout::$stdout
